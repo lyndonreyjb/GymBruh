2adcb3a1b051a9f09487572f0f3d8b2ae4e2d68c
Lyndon Rey Bualat <lyndonreybualat@gmail.com>
2023-11-15 00:20:44 -0700

    image update

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import { Search } from "../components";
 import WorkoutCard from "../components/WorkoutCard";
-
+import landing from "../assets/gym.jpg";
 const Exercises = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState("all");
 
-  const imageUrl =
-    "https://images.unsplash.com/photo-1571902943202-507ec2618e8f?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   console.log(exercises);
   return (
     <div>
-      <img src={imageUrl} alt="workout" className="object-cover h-96 w-full" />
+      <img src={landing} alt="workout" className="object-cover h-96 w-full" />
 
       <div className="mt-5">
         <Search
