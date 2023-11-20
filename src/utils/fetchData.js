67374e8a8cff2a094282dd5f3541a1ce21ed74aa
lyndonrey <lyndonreybualat@gmail.com>
2023-11-20 16:52:18 -0700

    fetch update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,8 +6,25 @@ export const exerciseOption = {
   },
 };
 
-export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
+export const fetchData = async (url, options, limit = 10) => {
+  // Add the limit query parameter to the URL
+  const urlWithLimit = new URL(url);
+  urlWithLimit.searchParams.append("limit", limit);
+
+  const response = await fetch(urlWithLimit, options);
   const data = await response.json();
   return data;
 };
+
+// Example usage to fetch 15 endpoints
+const url = "your_api_endpoint_here";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+const fetchedData = await fetchData(url, options, 15);
+console.log(fetchedData);
