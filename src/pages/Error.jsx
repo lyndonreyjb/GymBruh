0ba5fb71bec00ba8878c9148ceb9a789a4a6201e
Lyndon Rey Bualat <lyndonreybualat@gmail.com>
2023-11-23 23:43:18 -0700

    update for 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,23 @@
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <div className="grid w-full h-screen px-4 bg-white place-content-center">
+        <div className="text-center">
+          <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Uh-oh!
+          </p>
+
+          <p className="mt-4 text-gray-500">We cannot find that page.</p>
+
+          <a
+            href="/"
+            className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-black rounded hover:bg-neutral-700 focus:outline-none focus:ring">
+            Go Back Home
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
